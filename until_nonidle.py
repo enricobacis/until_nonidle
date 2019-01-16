26be401b25f0d78dc1feda57c7ec16a35bac2d41
enricobacis <enrico.bacis@gmail.com>
2019-01-16 17:49:47 +0100

    add script

---
+++ until_nonidle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import psutil
+import subprocess
+
+
+POLL_MILLISECONDS = 100
+
+
+def get_idletime():
+    return int(subprocess.check_output(["xprintidle"]))
+
+
+def kill_all(pid):
+    parent = psutil.Process(pid)
+    for child in parent.children(recursive=True):
+        child.kill()
+    parent.kill()
+
+
+def main():
+    last_idletime = 0
+    p = subprocess.Popen(sys.argv[1:])
+    retval = 1
+
+    while p.poll() is None:
+        # process is alive
+
+        current_idletime = get_idletime()
+
+        if current_idletime < last_idletime:
+            retval = 0
+            kill_all(p.pid)
+            p.wait()
+            break
+
+        last_idletime = current_idletime
+        time.sleep(POLL_MILLISECONDS / 1000.)
+
+    return retval
+
+
+if __name__ == "__main__":
+    sys.exit(main())
