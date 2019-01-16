b7907487cba0c6f421746098ae2d8d63daafb2ca
enricobacis <enrico.bacis@gmail.com>
2019-01-16 18:12:30 +0100

    add setup.py

--- README.rst
+++ README.rst
@@ -3,13 +3,21 @@ until_nonidle
 
 Execute something as long as the user is idling.
 
-requirements
+Description
+-----------
+
+The tools is especially useful in combination with auto lockscreens.
+The user can be notified (by a dimming action on the screen or by showing a
+text message on the display) about the fact that the computer is being locked,
+and can perform an action such as moving the mouse to dismiss the action.
+
+Requirements
 ------------
 
 -  xprintidle
 -  psutil
 
-example
+Example
 -------
 
 .. code::
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.rst') as README:
+    long_description = README.read()
+    long_description = long_description[long_description.index('Description'):]
+
+setup(
+    name='until_nonidle',
+    version='0.1',
+    description='Execute something as long as the user is idling.',
+    long_description=long_description,
+    install_requires=['psutils', 'xprintidle'],
+    url='http://github.com/enricobacis/until_nonidle',
+    author='Enrico Bacis',
+    author_email='enrico.bacis@gmail.com',
+    license='MIT',
+    packages=['until_nonidle'],
+    scripts=['scripts/until_nonidle'],
+    keywords='idle lock lockscreen action'
+)
